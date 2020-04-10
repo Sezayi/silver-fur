9b722b85349d310f9fd97b48f2d883be6ae01b8c
Sezayi Arslan <arslan.sezayi@gmail.com>
2020-04-10 15:46:44 +0200

    add font styling and npm update

--- src/pages/project.js
+++ src/pages/project.js
@@ -13,14 +13,14 @@ const ProjectPage = () => {
         <div className="flex justify-center w-full lg:w-6/12 ">
           <img className="lg:w-5/5" src="https://artifex.nu/storage/news-items/June2019/Bijzondere-aankoop-voor-de-sieradencollectie-van-Museum-Arnhem-header.jpg"></img>
         </div>
-        <div className="w-full p-8 lg:py-8 lg:px-24 lg:w-6/12">
-          <h1>Silver Faux Fur</h1>
+        <div className="content w-full p-8 lg:py-8 lg:px-24 lg:w-6/12">
+          <h1>SILVER FAUX FUR</h1>
           <p className="text-xl">Commodo try-hard chartreuse et drinking vinegar schlitz. Esse man bun dolore, ethical in reprehenderit live-edge affogato velit nostrud echo park. Sint poke offal, you probably haven't heard of them et poutine meggings consequat iceland disrupt occupy. Vinyl plaid activated charcoal cornhole small batch everyday carry slow-carb iPhone id air plant pop-up. Distillery keytar kitsch, hot chicken poke kogi fashion axe subway tile adaptogen unicorn you probably haven't heard of them tilde in cloud bread. Hoodie ut excepteur poke lorem labore esse vexillologist vinyl gochujang kale chips meggings in marfa mixtape.</p>
         </div>
       </section>
       <section className="flex flex-col-reverse lg:items-center mb-32 lg:flex lg:flex-row lg:mx-32 ">
-        <div className="w-full p-8 lg:py-8 lg:pl-32 lg:w-2/4">
-          <h1>Free Fur Knit Wear</h1>
+        <div className="content w-full p-8 lg:py-8 lg:pl-32 lg:w-2/4">
+          <h1>FUR KNIT WEAR</h1>
           <p className="text-xl">Commodo try-hard chartreuse et drinking vinegar schlitz. Esse man bun dolore, ethical in reprehenderit live-edge affogato velit nostrud echo park. Sint poke offal, you probably haven't heard of them et poutine meggings consequat iceland disrupt occupy. Vinyl plaid activated charcoal cornhole small batch everyday carry slow-carb iPhone id air plant pop-up. Distillery keytar kitsch, hot chicken poke kogi fashion axe subway tile adaptogen unicorn you probably haven't heard of them tilde in cloud bread. Hoodie ut excepteur poke lorem labore esse vexillologist vinyl gochujang kale chips meggings in marfa mixtape.</p>
         </div>
         <div className="w-full lg:w-2/4 lg:flex lg:justify-center ">
@@ -31,8 +31,8 @@ const ProjectPage = () => {
       <div className="flex justify-center w-full lg:w-6/12 ">
           <img className="lg:w-5/5" src="https://www.thefrenchjewelrypost.com/content/uploads/2017/08/barbara-langendijk-noon-passama-2.jpg"></img>
         </div>
-        <div className="w-full p-8 lg:py-8 lg:px-24 lg:w-6/12">
-          <h1>Arty silver wear</h1>
+        <div className="content w-full p-8 lg:py-8 lg:px-24 lg:w-6/12">
+          <h1>ARTY SILVER WEAR</h1>
           <p className="text-xl">Commodo try-hard chartreuse et drinking vinegar schlitz. Esse man bun dolore, ethical in reprehenderit live-edge affogato velit nostrud echo park. Sint poke offal, you probably haven't heard of them et poutine meggings consequat iceland disrupt occupy. Vinyl plaid activated charcoal cornhole small batch everyday carry slow-carb iPhone id air plant pop-up. Distillery keytar kitsch, hot chicken poke kogi fashion axe subway tile adaptogen unicorn you probably haven't heard of them tilde in cloud bread. Hoodie ut excepteur poke lorem labore esse vexillologist vinyl gochujang kale chips meggings in marfa mixtape.</p>
         </div>
       </section>
