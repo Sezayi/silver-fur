8bfab596e58cee8f7af4b743f0771fe0348f9c90
Sezayi Arslan <arslan.sezayi@gmail.com>
2020-05-03 20:40:29 +0200

    run noisejs from static

--- src/html.js
+++ src/html.js
@@ -1,6 +1,7 @@
 import React from "react"
 import PropTypes from "prop-types"
 
+
 export default function HTML(props) {
   return (
     <html {...props.htmlAttributes}>
@@ -11,7 +12,7 @@ export default function HTML(props) {
           name="viewport"
           content="width=device-width, initial-scale=1, shrink-to-fit=no"
         />
-        <script src="https://codepen.io/Tibixx/pen/BbBxRG.js">{}</script>
+        <script src="/static/d/noise.js">{}</script>
         {props.headComponents}
       </head>
       <body {...props.bodyAttributes}>
